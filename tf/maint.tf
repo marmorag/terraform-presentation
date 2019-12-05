8c85c7a0d341f44bf2f067bac8588f49a462aa8a
marmorag <g.marmo@hotmail.fr>
2019-12-05 01:53:44 +0100

    tf things

--- tf/maint.tf
+++ tf/maint.tf
@@ -20,12 +20,12 @@ resource "netlify_site" "tf-presentation" {
   name = "tf-presentation"
 
   repo {
-    repo_branch = "master"
-    command = "yarn build"
+    repo_branch   = "master"
+    command       = "yarn build"
     deploy_key_id = netlify_deploy_key.key.id
-    dir = "public"
-    provider = "github"
-    repo_path = "marmorag/terraform-presentation"
+    dir           = "public"
+    provider      = "github"
+    repo_path     = "marmorag/terraform-presentation"
   }
 }
 
