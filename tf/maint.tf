212a5bceaefef326a27a270654fb543b31b7f3ec
marmorag <g.marmo@hotmail.fr>
2019-12-05 01:52:32 +0100

    lot of tf things

--- tf/maint.tf
+++ tf/maint.tf
@@ -2,6 +2,18 @@ provider "netlify" {
   token = var.netlify_token
 }
 
+provider "github" {
+  token        = var.github_token
+  organization = var.github_organization
+}
+
+resource "github_repository_deploy_key" "key" {
+  title      = "Netlify"
+  repository = "terraform-presentation"
+  key        = netlify_deploy_key.key.public_key
+  read_only  = false
+}
+
 resource "netlify_deploy_key" "key" {}
 
 resource "netlify_site" "tf-presentation" {
@@ -15,4 +27,17 @@ resource "netlify_site" "tf-presentation" {
     provider = "github"
     repo_path = "marmorag/terraform-presentation"
   }
+}
+
+resource "github_repository_webhook" "webhook" {
+  repository = "terraform-presentation"
+  name       = "web"
+  events     = ["delete", "push", "pull_request"]
+
+  configuration {
+    content_type = "json"
+    url          = "https://api.netlify.com/hooks/github"
+  }
+
+  depends_on = [netlify_site.tf-presentation]
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,3 +1,11 @@
 variable "netlify_token" {
   description = "The netlify application token to authorize against provider"
 }
+
+variable "github_token" {
+  description = "The github auth token"
+}
+
+variable "github_organization" {
+  default = "marmorag"
+}
